e5262d67abe557415477717b85dc2f8c59c64497
Markus Karileet <markuskarileet@hotmail.com>
2018-02-22 07:01:28 +0200

    speechkit implementation example

---
+++ README.md
@@ -0,0 +1,4 @@
+# Usage
+First create an account in the [Nuance Developers](https://developer.nuance.com/public/index.php?task=register) site. Then open your account and find the **URL** and **APP_KEY** for your account. Add those to ```config.xml``` and ```package.json```
+
+Run on a device to test.
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Platform} from 'ionic-angular';
+import { SpeechKit } from '@ionic-native/speechkit';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public ttsInput: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private _platform: Platform,
+    private _speechKit: SpeechKit
+  ) { }
 
+  speak() {
+    this._platform.ready().then(() => {
+      this._speechKit.tts(this.ttsInput, 'ENG-GBR')
+        .then((s) => console.log(s))
+        .catch((e) => console.error(e));
+    });
   }
-
 }
