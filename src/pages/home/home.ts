fb8e1213dd129c79017d0b6a464526a57d54398f
Markus Karileet <markuskarileet@hotmail.com>
2018-10-04 22:28:31 +0300

    add voice parameter

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,7 +16,8 @@ export class HomePage {
 
   speak() {
     this._platform.ready().then(() => {
-      this._speechKit.tts(this.ttsInput, 'ENG-GBR')
+      // find voice names that match language from: https://developer.nuance.com/public/index.php?task=supportedLanguages
+      this._speechKit.tts(this.ttsInput, 'ENG-USA', 'Samantha')
         .then((s) => console.log(s))
         .catch((e) => console.error(e));
     });
